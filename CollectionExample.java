456ae431885a0514875170e9f7a910064bb7212d
Nisha Solanki <nishapsolanki@gmail.com>
2016-11-19 10:46:30 +0530

    Add files via upload

--- CollectionExample.java
+++ CollectionExample.java
@@ -11,20 +11,11 @@ class CollectionExample
 	}
 	public static void print(List list)
 	{
-		Iterator it= list.iterator();
-		/*for(String item : list )
+		//Iterator<String> it= new Iterator()
+		for(String item : list )
 		{
 			System.out.println(item);
-		}*/
-		while(it.hasNext())
-		{
-		   System.out.println(it.next());
 		}
 	}
-	public static void remove()
-	{
-	 
-	}
-	
 	
 }
